8a43b85c1b15167c5f28d9b02a40aa4f904be0a1
Nur Hayat Yavuz <101716477+nurhayatyavuz@users.noreply.github.com>
2025-05-26 14:34:13 +0300

    JavaScript_index

--- index.js
+++ index.js
@@ -1,18 +1,65 @@
-console.log("INDEX1 TRAINING STARTING >> STRING METHODS");
+console.log("first");
 
-let name1 = "jAvAsCrIpT, EğItImI";
-let name2 = "jAvAsCrIpT EğItImI";
+let sayi1 = 8;
+let sayi2 = 10;
+let toplam = sayi1 + sayi2;
+console.log(toplam, "sayilarin toplamı");
+console.log(typeof toplam, "sayiların türü");
 
-console.log(name1.length);
-console.log(name1.toUpperCase());
-console.log(name1.toLowerCase());
-console.log(name1.indexOf("EğItImI"));
-console.log(name1.lastIndexOf("EğItImI"));
-console.log(name1.slice(0, 5));
-console.log(name1.substring(0, 5));
-console.log(name1.replace("EğItImI", "kursu"));
-console.log(name1.split(","));
-console.log(name2.split(" "));
-console.log(name1.split(",")[0]);
-console.log(name1.trim());
-console.log(name1.charAt(3));
+let sayi3 = "6";
+let toplam2 = sayi1 + sayi3;
+console.log(toplam2, "bir number ve bir stringin toplamı?"
+);
+console.log(typeof toplam2);
+
+let toplam3 = sayi1 + Number(sayi3);
+
+let esitlik = sayi1 === sayi3;
+console.log(esitlik, "sayi1 ve sayi3 eşit mi?");
+
+const info = {
+  name: "nurhayat",
+  surname: "yavuz",
+  age: "23"
+};
+console.log(info);
+console.log(info.name);
+
+const arr = ["1", "2", "3", "4"];
+console.log(arr[0]);
+
+function firstFunc() {
+  console.log("fonksiyon");
+}
+firstFunc();
+
+const secondFunc = () => {
+  console.log("arrow function");
+};
+secondFunc();
+
+let sayi4 = 20;
+let sayi5 = 10;
+
+const sum = (num1, num2) => {
+  return num1 + num2;
+};
+
+let res = sum(sayi4, sayi5);
+console.log(res);
+
+function hello() {
+  console.log("selams");
+  console.log(this.name + this.val);
+}
+
+const obj = { name: "js", val: "20" };
+hello.call(obj);
+
+function total(a, b) {
+  return a + b;
+}
+
+const args = [3, 4];
+let sonuc = total.apply(null, args);
+console.log(sonuc, "toplam sonuç");
