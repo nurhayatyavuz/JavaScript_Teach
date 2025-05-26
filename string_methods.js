8b96d0cd1a96632efabea1f6f1e2e637dffb5160
Nur Hayat Yavuz <101716477+nurhayatyavuz@users.noreply.github.com>
2025-05-26 14:35:13 +0300

    JavaScript_StringMethods

---
+++ string_methods.js
@@ -0,0 +1,18 @@
+console.log("INDEX1 TRAINING STARTING >> STRING METHODS");
+
+let name1 = "jAvAsCrIpT, EğItImI";
+let name2 = "jAvAsCrIpT EğItImI";
+
+console.log(name1.length);
+console.log(name1.toUpperCase());
+console.log(name1.toLowerCase());
+console.log(name1.indexOf("EğItImI"));
+console.log(name1.lastIndexOf("EğItImI"));
+console.log(name1.slice(0, 5));
+console.log(name1.substring(0, 5));
+console.log(name1.replace("EğItImI", "kursu"));
+console.log(name1.split(","));
+console.log(name2.split(" "));
+console.log(name1.split(",")[0]);
+console.log(name1.trim());
+console.log(name1.charAt(3));
